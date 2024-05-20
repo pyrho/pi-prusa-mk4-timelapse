ba37e21d03883d777a3f8eaa77379e719c3ae8b2
pyrho <github@25.wf>
2024-05-20 20:06:05 +0000

    cli flags instead of config

--- cmd/timelapse-serial/main.go
+++ cmd/timelapse-serial/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/pyrho/timelapse-serial/internal/camera"
-	"github.com/pyrho/timelapse-serial/internal/config"
 	"github.com/pyrho/timelapse-serial/internal/interrupt_trap"
 	"github.com/pyrho/timelapse-serial/internal/serial"
 )
 
-type Command int
-
 func main() {
-	config := config.LoadConfig()
-	camera := camera.MakeCameraWrapper(config.OutputDir)
+	portName := flag.String("portName", "/dev/ttyACM0", "The path of the printer port")
+	baudRate := flag.Int("baudRate", 115200, "The baud rate of the serial port")
+	outputDir := flag.String("outputDir", "/tmp/timelapse-serial-captures", "The output path where the pictures and timelapses will be stored")
+	flag.Parse()
+
+	camera := camera.MakeCameraWrapper(*outputDir)
 
 	interrupttrap.TrapInterrupt(func() { camera.Stop() })
 
 	serial.StartSerialRead(
-		config.BaudRate,
-		config.SerialPort,
+		*baudRate,
+		*portName,
 		serial.CreateSerialMessageHandler(&camera),
 	)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/pyrho/timelapse-serial
 go 1.22
 
 require (
-	github.com/BurntSushi/toml v1.3.2
 	github.com/jonmol/gphoto2 v1.0.1
 	go.bug.st/serial v1.6.2
 )
