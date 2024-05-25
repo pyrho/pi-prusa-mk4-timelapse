10b37fba7561e54de7eeaa55e07e33673f9babdf
pyrho <github@25.wf>
2024-05-25 13:18:25 +0000

    image: error print

--- internal/web/image.go
+++ internal/web/image.go
@@ -15,24 +15,34 @@ func CreateAndSaveThumbnail(imgPath string) string {
 	m1 := regexp.MustCompile(`snap([0-9]+.jpg)`)
 	thumbPath := m1.ReplaceAllString(imgPath, "thumb${1}")
 	if _, err := os.Stat(thumbPath); !errors.Is(err, fs.ErrNotExist) {
+        // Thumbnail already exists
 		return thumbPath
 	}
 
 	image, err := vips.NewImageFromFile(imgPath)
 	if err != nil {
-		panic(err)
+		log.Println("Cannot Open image from file", imgPath)
+		return ""
 	}
-    defer image.Close()
+	defer image.Close()
 
 	if err := image.Resize(0.06, vips.KernelLinear); err != nil {
-		panic(err)
+		log.Println("Cannot resize image", err)
+		return ""
 	}
 	buf, _, err := image.ExportJpeg(&vips.JpegExportParams{
 		Quality: 90,
 	})
+
+	if err != nil {
+		log.Println("Cannot export JPEG file", err)
+		return ""
+	}
+
 	err = os.WriteFile(thumbPath, buf, 0644)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Cannot write JPEG file", err)
+		return ""
 	}
 	return thumbPath
 }
